1a3929e94793631045fca0825a19d01bcce52f41
Jonas Valbjørn Andersen <dotjonas@gmail.com>
2015-01-09 17:30:50 +0100

    Basic ideas
    first stab

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      meanValue <- NULL                                       #Set the mean to NULL
+      set <- function(y){
+            x <- y                                         # '<<-' searches parent env for definition
+            meanvalue <<- NULL
+      }
+      get <- function() x
+      setmatrix <- function(solve) meanValue <<- solve
+      getmatrix <- function() meanValue
+      list(set = set, get = get,
+           setmatrix = setmatrix,
+           getmatrix = getmatrix)
 }
 
 
@@ -12,4 +22,18 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+}
+
+
+
+cacheSolve <- function(x=matrix(), ...) {
+      m<-x$getmatrix()
+      if(!is.null(m)){
+            message("getting cached data")
+            return(m)
+      }
+      matrix<-x$get()
+      m<-solve(matrix, ...)
+      x$setmatrix(m)
+      m
 }
